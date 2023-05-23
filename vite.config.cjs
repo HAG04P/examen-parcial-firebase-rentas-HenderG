56774f90d98b4afe571c47870d04947a4451f738
Hender <est.hguillenp0201@uml.edu.ni>
2023-05-22 20:21:37 -0600

    proceso de compilacion

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
 const { resolve } = require('path')
+//Add env variable to check if is production or development
+
 const env = process.env;
-env.mode = env.mode || "production";
+//Set a base route if deploying on github pages
+//env.mode = env.mode || "production";
+
+console.log('Environment: ', env.mode);
+
 export default defineConfig({
   root: "./",
-  //:home: En esta linea se cambia la frase /eventos-app/ por el nombre de su repositorio
+  //Add base route if deployiing on production
   base: env.mode === "production" ? "/examen-parcial-firebase-rentas-HenderG/" : "/",
+
   build: {
     outDir: "docs",
     assetsDir: "assets",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html')
+        main: resolve(__dirname, 'index.html'),
       }
     }
   },
